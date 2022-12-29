fa8121d993d4287d61392c0105e528dde128c3f4
Mikhail Kaufmann <mihail.kaufmann@gmail.com>
2022-12-29 18:44:50 +0300

    По гайду: с ошибкой из-за асинхронности

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState("BTC");
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toCurrency, setToCurrency] = React.useState("USDT");
+  const [toPrice, setToPrice] = React.useState(0);
+  const [rates, setRates] = React.useState({});
+
+  React.useEffect(() => {
+    fetch(
+      "http://api.coinlayer.com/api/live?access_key=fc057f247e9c26e5ca1f3c335b9bf92a"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.rates);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Не удалось получить информацию");
+      });
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / rates[fromCurrency];
+    const res = price * rates[toCurrency];
+    setToPrice(res);
+    setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+    const price = value / rates[toCurrency];
+    const res = price * rates[fromCurrency];
+    setToPrice(value);
+    setFromPrice(res);
+  };
+
+  const onChangeFromCurrency = (cur) => {
+    setFromCurrency(cur);
+    onChangeFromPrice(fromPrice);
+  };
+
+  const onChangeToCurrency = (cur) => {
+    setToCurrency(cur);
+    onChangeToPrice(toPrice);
+  };
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={onChangeFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={onChangeToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
--- src/Block.jsx
+++ src/Block.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP'];
+const defaultCurrencies = ['BTC', 'USDT', 'ETC', 'ETH'];
 
 export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
   <div className="block">
